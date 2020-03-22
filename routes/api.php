81102566a63a28dc565706e85d0d02e050537183
wt2209 <wt2209@126.com>
2020-03-22 16:04:53 +0800

    集成swagger-ui

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('auth/login', 'Api\UserController@login')->name('api.auth.login');
 Route::namespace('Api')
     ->name('api.')
     ->middleware('auth:api')
